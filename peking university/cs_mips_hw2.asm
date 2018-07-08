575efb8e1e6b6579ba7914763cf44ba188581637
polarisZhao <zhaozhichao.ml@gmail.com>
2018-07-08 18:30:26 +0800

    update cs_mips_hw2

--- peking university/cs_mips_hw2.asm
+++ peking university/cs_mips_hw2.asm
@@ -5,9 +5,11 @@
 
 # data segment
     .data
-string: .byte 0:100       
-find: .asciiz "Success! Location:"
-not_find: .asciiz "Fail!"
+string: .byte 0:100 # string len
+size: .word  100       
+find: .asciiz "Success! Location:" # find success statement
+not_find: .asciiz "Fail!"  # find fail statement
+
 
 # test segment
     .text       
@@ -15,38 +17,40 @@ not_find: .asciiz "Fail!"
 main:
     li $s1, '\n'
     la $a0, string
+    la $a1,size
+    lw $a1,0($a1)
 
-    li $v0, 8  # input a string
+    li $v0, 8   # input a string
     syscall
 read:
-    li $v0,12  # input a char
+    li $v0,12   # input a char
     syscall
     beq $v0,'?', end     # end the program
     li $t1, 0
-loop:
-    lb $s3, string($t1) 
-    addi $t1, $t1, 1  
-    beq $v0, $s3, success  # search success
-    beq $s1, $s3, fail  # search fail
+loop:  # search char in string
+    lb $s3, string($t1) # get offset char
+    addi $t1, $t1, 1  # offset +1
+    beq $v0, $s3, success  # search char success
+    beq $s1, $s3, fail  # search char fail
     j loop
 
 fail:
-    la $a0, not_find  # statement
+    la $a0, not_find  # print statement
     li $v0, 4
     syscall
-    move $a0, $s1 # \n
+    move $a0, $s1 # print CR
     li $v0, 11
     syscall
     j read
  
 success:
-    la $a0, find # statement
+    la $a0, find # print statement
     li $v0, 4
     syscall
-    move $a0, $t1 # location  
+    move $a0, $t1 # print location  
     li $v0,1
     syscall  
-    move $a0, $s1 # \n
+    move $a0, $s1 # print CR
     li $v0, 11
     syscall
     j read
